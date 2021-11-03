cfdad6fe38f7d12a69d2eb8ef0344ffc95de68e0
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-03 19:45:44 +0000

    add event handler  and stdin funs

--- play.js
+++ play.js
@@ -6,20 +6,52 @@ const connect = function () {
     port: 50541,
     host: "165.227.47.243",
   });
-  
-  conn.on('connect',function(){
-    console.log('client is connected to server.');
-    });
-    conn.on('connect', () => {
-      conn.write('Name: KRV');
-      });
-   conn.setEncoding("utf8");
-   return conn;
+
+  // conn.on('connect', function () {
+  //   console.log('client is connected to server.');
+  // });
+  // conn.on('connect', () => {
+  //   conn.write('Name: KRV');
+  // });
+  conn.setEncoding("utf8");
+  return conn;
 };
 
-console.log("Connecting ...");  
+// setup interface to handle user input from stdin
 
-module.exports = {connect}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+
+  // stdin.on('data', (key) => {
+  //   process.stdout.write('.');
+  // });
+  stdin.on("data", function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+    
+  }); 
+// add an else if u want
+  // else{
+    //   process.stdout.write('.');
+    // }
+
+
+  return stdin;
+
+
+
+};
+
+
+ setupInput();
+console.log("Connecting ...");
+
+module.exports = { connect , setupInput }
 
 
 
