9bbe76a782600db4d4617abe390a177f39ca1a35
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-03 19:21:32 +0000

    add set interval to move up snake

--- client.js
+++ client.js
@@ -1,7 +1,38 @@
 
-const {connect} = require("./play.js");
+const { connect } = require("./play.js");
 
-const con=connect();
-con.on('data', function(message){ // this is how we receive
+const con = connect();
+con.on('data', function (message) { // this is how we receive
   console.log(`Server says: ${message}`);
+});
+
+ con.on('connect', function () {
+    console.log('client is connected to server.');
   });
+  con.on('connect', () => {
+    con.write('Name: KRV');
+  });
+
+// con.on('connect', () => {
+//   con.write('Move: up');
+// });
+// =====================================set time out
+// let timer=0;
+// for(let i=0;i<50;i++){
+// con.on('connect', () => {  
+//   setTimeout(()=>{
+//     con.write('Move: up');
+//   },
+//   timer+=500);
+// });
+// }
+// ==============================/
+let timer=0;
+
+con.on('connect', () => {  
+    setInterval(()=>{
+      con.write('Move: up');
+    },
+    timer+=200);
+  });
+
