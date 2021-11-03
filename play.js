1a06bfc17a910b11ff262d6c26635b1b530d8ef6
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-03 18:46:30 +0000

    add name krv

--- play.js
+++ play.js
@@ -6,8 +6,14 @@ const connect = function () {
     port: 50541,
     host: "165.227.47.243",
   });
-
-  conn.setEncoding("utf8");
+  
+  conn.on('connect',function(){
+    console.log('client is connected to server.');
+    });
+    conn.on('connect', () => {
+      conn.write('Name: KRV');
+      });
+   conn.setEncoding("utf8");
    return conn;
 };
 
